a7c6f8f0514f0f460c885bb0909b2239722414e3
Esai Morales <esaimn96@gmail.com>
2018-03-15 14:00:37 -0700

    nb vs lr

--- naivebayes.py
+++ naivebayes.py
@@ -12,7 +12,7 @@ train = train_data[:100000]
 test = train_data[100000:]
 
 train_y = train['toxic']
-test_y = test['toxic']
+test_y = test['identity_hate']
 
 # print train_y
 
@@ -20,64 +20,55 @@ train_text = train['comment_text']
 test_text = test['comment_text']
 all_text = pd.concat([train_text, test_text])
 
-for i, value in enumerate(train['toxic']):
-    if value == 1:
-        print train['comment_text'][i]
+# for i, value in enumerate(train['toxic']):
+#     if value == 1:
+#         print train['comment_text'][i]
 
 vectorizer = TfidfVectorizer(
     strip_accents = 'unicode',
     stop_words = 'english',
-    max_features = 10000
+    max_features = 1000
 )
 
-train_vectorized = vectorizer.fit_transform(train_text)
+vectorizer.fit(all_text)
+train_vectorized = vectorizer.transform(train_text)
 # print train_vectorized.toarray()
-# print train_vectorized.toarray().shape
 
 test_vectorized = vectorizer.transform(test_text)
+# print test_vectorized.toarray()
+
+# train_vectorized = vectorizer.fit_transform(train_text)
+# print train_vectorized.toarray()
+# print train_vectorized.toarray().shape
+#
+# test_vectorized = vectorizer.transform(test_text)
 # print test_vectorized.toarray()
 # print test_vectorized.toarray().shape
 
 vectorized_words = vectorizer.get_feature_names()
-print vectorized_words
+# print vectorized_words
 
-model = LogisticRegression()
-model.fit(train_vectorized, train_y)
+model = MultinomialNB()
+# model.fit(train_vectorized, train_y)
 
-prediction = model.predict(test_vectorized)
+# prediction = model.predict(test_vectorized)
 
-print len(prediction), len(test_y)
-print test_y
+# print len(prediction), len(test_y)
 
-# count = 0
-# for i, value in enumerate(prediction):
-#     if value == test_y[i]:
-#         count += 1
+categories = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+prediction = []
 
-# print 'hit:', count
-# print 'miss:', len(prediction) - count
-# print float(count)/float(len(prediction))
+for category in categories:
+    print 'predicting', category ,'...'
+    model.fit(train_vectorized, train[category])
+    prediction = model.predict(test_vectorized)
+    print 'accuracy_score:', accuracy_score(test[category], prediction)
 
-
-print 'ACCURACY:', accuracy_score(test_y, prediction)
+# print 'ACCURACY:', accuracy_score(test_y, prediction)
 
 #verify
-
-# print train_text
-# print prediction[:30]
-
-# 2 features
-# MNB   0.904618018835
-# LR    0.904618018835
-
-# 5 features
-# MNB   0.904618018835
-# LR    0.904618018835
-
-# 1000 features
-# MNB   0.944620704705
-# LR    0.949488845243
-
-# 10000 features
-# MNB   0.948330563529
-# LR    0.955464907421
+# for i, row in enumerate(test_text):
+#     if prediction[i] == 1:
+#         print 'text:', row
+#         print 'prediction:', prediction[i]
+#         print 'actual:', np.array(test_y)[i]
